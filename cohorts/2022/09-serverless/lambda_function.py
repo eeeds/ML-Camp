fd0e260c05bfaea64b2202d29981db46979ca9f0
eeeds <encinaesteban27@gmail.com>
2022-11-25 05:42:54 -0300

    Q5

---
+++ cohorts/2022/09-serverless/lambda_function.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+import tensorflow.lite as tflite
+from io import BytesIO
+from urllib import request
+from PIL import Image
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+def prepare_input(x):
+    return x*(1./255)
+
+def predict(url):
+    img = download_image(url)
+    img = prepare_image(img, target_size=(150, 150))
+
+    x = np.array(img, dtype='float32') 
+    X = np.array([x])
+    X = prepare_input(X)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    return float(preds[0][0])
+
+def lambda_handler(event, context):
+    url = event['url']
+    pred = predict(url)
+    
+    result = {'prediction': pred}
+
+    return result
+    
+interpreter = tflite.Interpreter(model_path='dino_dragon_10_0.899.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+#url: https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/1280px-Smaug_par_David_Demaret.jpg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
